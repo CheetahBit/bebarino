e459e2b4dbcd26948b344e6f783814efa0c1d134
Rasoul <rasoulmousavidev@gmail.com>
2023-03-07 18:11:59 +0330

    debug

--- config/telegram.php
+++ config/telegram.php
@@ -3,6 +3,7 @@
 return json_decode(json_encode([
     "token" => env("BOT_TOKEN"),
     "channel" => "bebarinoTest",
+    "bot" => "follow4u_bot",
     "admins" => [130912163],
     "flows" => [
         "beginning" => [
