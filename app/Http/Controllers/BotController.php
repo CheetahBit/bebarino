f918c1676740822cd8a14e0b8ef47485098c0f6d
Rasoul <rasoulmousavidev@gmail.com>
2023-03-08 00:52:13 +0330

    debug

--- app/Http/Controllers/BotController.php
+++ app/Http/Controllers/BotController.php
@@ -98,7 +98,7 @@ class BotController extends Controller
         Http::get('https://api.telegram.org/bot' . $token . '/setwebhook?url=https://bot.cheetahbit.org/api/bot');
 
         $trips = Trip::where('messageId', '<>', null)->where('date', '>=', Carbon::today()->format('Y/m/d'))
-            ->orderBy('asc', 'date')->groupBy(function ($val) {
+            ->orderBy('date', 'asc')->groupBy(function ($val) {
                 return Carbon::parse($val->date)->format('Y');
             })->groupBy(function ($val) {
                 return Carbon::parse($val->date)->format('m');
