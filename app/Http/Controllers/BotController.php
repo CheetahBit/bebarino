f2fbf95e6b9d93e4c99f3f517051f3e659a9cfbf
Rasoul <rasoulmousavidev@gmail.com>
2023-03-08 17:16:40 +0330

    debug

--- app/Http/Controllers/BotController.php
+++ app/Http/Controllers/BotController.php
@@ -99,6 +99,6 @@ class BotController extends Controller
         Http::get('https://api.telegram.org/bot' . $token . '/setwebhook?url=https://bot.cheetahbit.org/api/bot');
 
 
-        return Carbon::today()->lte(Carbon::parse("2025/01/01")) ? 'before': 'after';
+        return Carbon::parse("2025/01/01")->lte(Carbon::today()) ? 'before': 'after';
     }
 }
