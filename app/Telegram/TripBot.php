5ff7f4f35df0ba5b87abd118a02ef67adf5793c9
Rasoul <rasoulmousavidev@gmail.com>
2023-03-08 17:27:07 +0330

    debug

--- app/Telegram/TripBot.php
+++ app/Telegram/TripBot.php
@@ -246,7 +246,7 @@ class TripBot
         $userId = $result->userId;
         $trip = $result->data;
 
-        if ($config->keywords->desire == $trip->ticket) $trip->ticket = $config->keywords->notEntered;
+        if (!isset($trip->ticket)) $trip->ticket = $config->keywords->notEntered;
 
         $this->api->chat($userId)->sendMessage()->text('confirmTrip', (array)$trip)->inlineKeyboard()->rowButtons(function ($m) {
             $m->button('confirm', 'data', 'Trip.submit');
