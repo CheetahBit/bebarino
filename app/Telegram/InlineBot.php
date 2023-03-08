7ca001926a31e62ca8cc4b46b28f594145fb3a62
Rasoul <rasoulmousavidev@gmail.com>
2023-03-08 18:09:11 +0330

    debug

--- app/Telegram/InlineBot.php
+++ app/Telegram/InlineBot.php
@@ -51,18 +51,19 @@ class InlineBot
                 $requests = $packages->merge($trips)->sortByDesc('updated_at');
                 foreach ($requests as $request) {
                     $request = collect($request);
-                    $type = (isset($request->date) ? 'trip' : 'package');
-                    $title = $keywords->{$type} . " - " .  $request->fromCountry . " , " . $request->fromCity . " > " . $request->toCountry . " , " . $request->toCity;
-                    $results[] = [
-                        'type' => 'article',
-                        'title' => $title,
-                        'description' => ($request->date ?? '') . " " . $request->desc,
-                        'input_message_content' => ['message_text' => 'show' . ucfirst($type) . "-" . $request->id],
-                        'id' => $type.$request->id,
-                        'packages' => $packages->toArray(),
-                        'trips' => $trips->toArray(),
+                    Log::alert($request);
+                    // $type = (isset($request->date) ? 'trip' : 'package');
+                    // $title = $keywords->{$type} . " - " .  $request->fromCountry . " , " . $request->fromCity . " > " . $request->toCountry . " , " . $request->toCity;
+                    // $results[] = [
+                    //     'type' => 'article',
+                    //     'title' => $title,
+                    //     'description' => ($request->date ?? '') . " " . $request->desc,
+                    //     'input_message_content' => ['message_text' => 'show' . ucfirst($type) . "-" . $request->id],
+                    //     'id' => $type.$request->id,
+                    //     'packages' => $packages->toArray(),
+                    //     'trips' => $trips->toArray(),
 
-                    ];
+                    // ];
                 }
                 break;
 
