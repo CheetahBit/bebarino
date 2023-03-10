292427846d835e6a4ea8b568ece8ed71fb8422f7
Rasoul <rasoulmousavidev@gmail.com>
2023-03-11 00:41:12 +0330

    debug

--- app/Jobs/MessageHandle.php
+++ app/Jobs/MessageHandle.php
@@ -32,40 +32,34 @@ class MessageHandle implements ShouldQueue
      */
     public function handle(): void
     {
-        try {
+        $config = config('telegram');
+        $action = new stdClass;
 
-
-            $config = config('telegram');
-            $action = new stdClass;
-
-            if (isset($this->message->text)) {
-                $text = $this->message->text;
-                $text = str_replace(['۰', '۱', '۲', '۳', '۴', '۵', '۶', '۷', '۸', '۹'], ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'], $text);
-                if (str_contains($text, 'package-'))
-                    $text = "requestPackage";
-                else if (str_contains($text, 'trip-'))
-                    $text = "requestTrip";
-                $text = array_search($this->message->text, (array) $config->keywords) ?: $text;
-                if (array_key_exists($text, (array) $config->actions))
-                    $action = $config->actions->{$text};
-            }
-
-            if (!isset($action->class)) {
-                $userId = $this->message->from->id;
-                $cache  = json_decode(Cache::get($userId, '{}'));
-                $action = $config->actions->{$cache->action};
-            }
-
-            $this->message->type = "this->message";
-            $class = new ("App\Telegram\\" . $action->class . "Bot")($this->message);
-            $class->{$action->method}();
-        } catch (Exception $th) {
-            Log::error($th->getMessage());
+        if (isset($this->message->text)) {
+            $text = $this->message->text;
+            $text = str_replace(['۰', '۱', '۲', '۳', '۴', '۵', '۶', '۷', '۸', '۹'], ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'], $text);
+            if (str_contains($text, 'package-'))
+                $text = "requestPackage";
+            else if (str_contains($text, 'trip-'))
+                $text = "requestTrip";
+            $text = array_search($this->message->text, (array) $config->keywords) ?: $text;
+            if (array_key_exists($text, (array) $config->actions))
+                $action = $config->actions->{$text};
         }
+
+        if (!isset($action->class)) {
+            $userId = $this->message->from->id;
+            $cache  = json_decode(Cache::get($userId, '{}'));
+            $action = $config->actions->{$cache->action};
+        }
+
+        $this->message->type = "this->message";
+        $class = new ("App\Telegram\\" . $action->class . "Bot")($this->message);
+        $class->{$action->method}();
     }
 
 
-    public function failed($exception)
+    public function failed(Exception $exception)
     {
         Log::error(var_dump($exception));
     }
