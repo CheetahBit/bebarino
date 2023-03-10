5052b54b5e5a5e65c1dfc00977c3d034aa8ecca9
Rasoul <rasoulmousavidev@gmail.com>
2023-03-10 23:33:07 +0330

    debug

--- config/telegram.php
+++ config/telegram.php
@@ -1,15 +1,23 @@
 <?php
 
-return json_decode(json_encode([
+require './config/telegram/actions.php';
+require './config/telegram/flows.php';
+require './config/telegram/keywords.php';
+require './config/telegram/messages.php';
+require './config/telegram/optionals.php';
+
+
+return collect([
     "token" => env("BOT_TOKEN"),
     "channel" => "bebarinoTest",
     "bot" => "follow4u_bot",
     // "admins" => [1613366049],
     "admins" => [130912163],
     "support" => 1613366049,
-    require './config/bot/actions.php',
-    require './config/bot/flows.php',
-    require './config/bot/keywords.php',
-    require './config/bot/messages.php',
-    require './config/bot/optionals.php',
-]));
+    
+    "actions" => $actions,
+    "flows" => $flows,
+    "keywords" => $keywords,
+    "messages" => $messages,
+    "optionals" => $optionals,
+]);
