3a1209d454e1614c8e60fafaea675dd2728de158
Rasoul <rasoulmousavidev@gmail.com>
2023-03-11 01:24:48 +0330

    debug

--- app/Http/Controllers/BotController.php
+++ app/Http/Controllers/BotController.php
@@ -41,7 +41,7 @@ class BotController extends Controller
     public function download($folder, $name)
     {
         $request = request();
-        Log::alert('Request For Download : ' . dd($request));
+        Log::alert('Request For Download : ' . print_r($request));
         return Storage::download($folder . '/' . $name, $name . '.jpg');
     }
 
