110ad50006351097aee6b7c125a733c1c0738cfb
Rasoul <rasoulmousavidev@gmail.com>
2023-03-10 23:27:47 +0330

    debug

--- app/Http/Controllers/BotController.php
+++ app/Http/Controllers/BotController.php
@@ -59,6 +59,6 @@ class BotController extends Controller
 
         // Http::get('https://api.telegram.org/bot' . $token . '/setwebhook?url=https://bot.cheetahbit.org/api/bot');
     
-        return json_encode(config('telegram'));
+        return config('telegram')->keywords;
     }
 }
